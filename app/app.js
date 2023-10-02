001efe50c2fbbe824ce816d42fde2554ba11bb19
Millie Ellis <ellismillie92@gmail.com>
2023-10-02 12:03:25 +0100

    completed task 3

--- app/app.js
+++ app/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const { getTopics } = require('./controllers/topics.controllers')
+const { getEndpoints } = require('./controllers/endpoints.controllers')
 const { handleServerErrors } = require('./controllers/errors.controllers')
 
 const app = express()
 
 app.get('/api/topics', getTopics)
+
+app.get('/api', getEndpoints)
 
 app.all("/*", (req, res, next) => {
     res.status(404).send({ message: 'Path not found'})
