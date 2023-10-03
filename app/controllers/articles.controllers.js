fe6c919486301c069e8ef24ed4a7338aa7637a36
Millie Ellis <ellismillie92@gmail.com>
2023-10-03 10:43:04 +0100

    Task 6 completed and changes merged

--- app/controllers/articles.controllers.js
+++ app/controllers/articles.controllers.js
@@ -1,5 +1,5 @@
 const { fetchArticles } = require('../models/articles.models')
-const { fetchArticleByID } = require('../models/articles.models')
+const { fetchArticleById } = require('../models/articles.models')
 
 exports.getArticles = (req, res, next) => {
     fetchArticles().then((articles) => {
@@ -7,10 +7,10 @@ exports.getArticles = (req, res, next) => {
     })
 }
 
-exports.getArticleByID = (req, res, next) => {
+exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params
 
-    fetchArticleByID(article_id).then((article) => {
+    fetchArticleById(article_id).then((article) => {
         res.status(200).send({article})
     })
     .catch((err) => {
