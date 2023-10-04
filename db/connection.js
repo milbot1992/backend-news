6a7c38aa32e1df579df33a94d7a84935a6b0ae9c
Millie Ellis <ellismillie92@gmail.com>
2023-10-04 11:05:29 +0100

    moved listen.js to top folder

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || 'production';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
